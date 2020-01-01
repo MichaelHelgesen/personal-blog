57ac5f3730946760cf5660a353ed85405919bee6
Michael Helgesen <michaelhelgesen02@gmail.com>
2020-01-01 19:09:04 +0100

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Mikke Learns</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <h2>I'm Mikke, trying to be a front end developer.</h2>
+        </div>
+    )
+}
+
+export default IndexPage
