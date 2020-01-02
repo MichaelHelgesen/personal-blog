cd501b4a129c3bce5a83ba0bced6f1bd9901d2dc
Michael Helgesen <michaelhelgesen02@gmail.com>
2020-01-02 23:01:37 +0100

    Updates

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,53 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Link, graphql, useStaticQuery} from "gatsby";
 
 const BlogPage = () => {
+
+    const blogData = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        date
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    }
+    `);
+
+    console.log(blogData);
+
+    const Blogdata = () => {
+        return (
+            <ol>
+                {blogData.allMarkdownRemark.edges.map((item) => (
+                        <li>
+                            <h2>
+                                <Link to={`blog/${item.node.fields.slug}`}>
+                                    {item.node.frontmatter.title}
+                                </Link>
+                            </h2>
+                            <p>{item.node.frontmatter.date}</p>
+                            <p>{item.node.fields.slug}</p>
+                        </li>
+                    )
+                )}
+            </ol>
+        )
+    }
+
     return (
         <Layout>
             <h1>Blog</h1>
-            <p>Ports will show up here.</p>
+            <Blogdata />
+            
         </Layout>
         
     )
