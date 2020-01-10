e716558eb901c8589678b0b549aec909c9a13285
Michael Helgesen <michaelhelgesen02@gmail.com>
2020-01-10 21:48:20 +0100

    Updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,14 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
 
-require("./src/styles/prism-dark.css");
+// You can delete this file if you're not using it
+
+require("prism-themes/themes/prism-atom-dark.css");
+require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+
+exports.onInitialClientRender = () => {
+  require("typeface-nunito");
+};
