17f63955bc4380f4497767ccfee4a935e9edbb3a
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-03-03 14:27:14 +0100

    update

--- src/components/head.js
+++ src/components/head.js
@@ -20,7 +20,7 @@ const Head = ({ title }) => {
         
         <Helmet>
                 <title>{`${title} | ${data.site.siteMetadata.title}`}</title>
-                <link rel="canonical" href="http://mikkesblogg.no" />
+                <link rel="canonical" href="https://mikkesblogg.no" />
                 < meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" charSet="utf-8"/>
             </Helmet>
         
