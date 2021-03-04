92e599eef1b0648dcf68bdd05e7cf68063ac32cc
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-03-04 11:03:45 +0100

    update

--- src/components/head.js
+++ src/components/head.js
@@ -21,7 +21,7 @@ const Head = ({ title, description }) => {
         <Helmet htmlAttributes={{ lang : "no" }}>
                 <title>{`${title} | ${data.site.siteMetadata.title}`}</title>
                 <link rel="canonical" href="https://www.mikkesblogg.no/" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <meta name="description" content={description}/>
         </Helmet>
         
