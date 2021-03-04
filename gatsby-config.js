9cc33169148b158e97907893e7daa9feb7425e01
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-03-04 09:02:41 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,35 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-sitemap`,
-  },
+      options: {
+        query: `
+          {
+            wp {
+              generalSettings {
+                siteUrl
+              }
+            }
+  
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+        }`,
+        resolveSiteUrl: ({site, allSitePage}) => {
+          //Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+          return site.wp.generalSettings.siteUrl
+        },
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.nodes.map(node => {
+            return {
+              url: `${site.wp.generalSettings.siteUrl}${node.path}`,
+              changefreq: `never`,
+              priority: 0.7,
+            }
+          })
+      }
+    },
     "gatsby-plugin-react-helmet",
   {
     resolve: "gatsby-source-contentful",
